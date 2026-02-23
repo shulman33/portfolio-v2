f81b699eabf83878a50dc02b5796d3f1a32c3d11
Sam Shulman <sshulma5@mail.yu.edu>
2026-02-23 14:29:13 -0500

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Syne, Fragment_Mono, DM_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const syne = Syne({
@@ -56,6 +57,7 @@ export default function RootLayout({
           Skip to main content
         </a>
         {children}
+        <Analytics />
       </body>
     </html>
   );
